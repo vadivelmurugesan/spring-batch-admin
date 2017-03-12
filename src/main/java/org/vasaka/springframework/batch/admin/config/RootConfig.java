2563ff03d392221acd75c133e4024b75887fbf65
Vadivel Murugesan <teamvasaka@gmail.com>
2017-03-12 22:01:39 +0000

    Integrated with spring-batch-admin

--- src/main/java/org/vasaka/springframework/batch/admin/config/RootConfig.java
+++ src/main/java/org/vasaka/springframework/batch/admin/config/RootConfig.java
@@ -1,10 +1,76 @@
 package org.vasaka.springframework.batch.admin.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.batch.admin.service.JobService;
+import org.springframework.batch.admin.service.SimpleJobServiceFactoryBean;
+import org.springframework.batch.core.configuration.JobRegistry;
+import org.springframework.batch.core.configuration.support.MapJobRegistry;
+import org.springframework.batch.core.explore.JobExplorer;
+import org.springframework.batch.core.explore.support.JobExplorerFactoryBean;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.support.SimpleJobLauncher;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.repository.support.JobRepositoryFactoryBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 @ComponentScan(basePackages = "org.vasaka.springframework.batch.admin.service")
+@Import(DataSourceConfig.class)
 public class RootConfig {
+
+	@Autowired
+	private DataSource dataSource;
+
+	@Autowired
+	private PlatformTransactionManager transactionManager;
+
+	@Bean
+	public JobService jobService() throws Exception {
+		SimpleJobServiceFactoryBean factoryBean = new SimpleJobServiceFactoryBean();
+		factoryBean.setDataSource(dataSource);
+		factoryBean.setJobExplorer(jobExplorer());
+		factoryBean.setJobRepository(jobRepository());
+		factoryBean.setJobLauncher(jobLauncher());
+		factoryBean.setJobLocator(jobRegistry());
+		factoryBean.setTransactionManager(transactionManager);
+		factoryBean.afterPropertiesSet();
+		return factoryBean.getObject();
+	}
+
+	@Bean
+	public JobRepository jobRepository() throws Exception {
+		JobRepositoryFactoryBean factory = new JobRepositoryFactoryBean();
+		factory.setDataSource(dataSource);
+		factory.setTransactionManager(transactionManager);
+		factory.afterPropertiesSet();
+		return (JobRepository) factory.getObject();
+	}
+
+	@Bean
+	public JobExplorer jobExplorer() throws Exception {
+		JobExplorerFactoryBean factory = new JobExplorerFactoryBean();
+		factory.setDataSource(dataSource);
+		factory.afterPropertiesSet();
+		return factory.getObject();
+	}
+
+	@Bean
+	public JobRegistry jobRegistry() {
+		return new MapJobRegistry();
+	}
+
+	@Bean
+	public JobLauncher jobLauncher() throws Exception {
+		SimpleJobLauncher jobLauncher = new SimpleJobLauncher();
+		jobLauncher.setJobRepository(jobRepository());
+		jobLauncher.afterPropertiesSet();
+		return jobLauncher;
+	}
 
 }
--- src/main/java/org/vasaka/springframework/batch/admin/config/WebConfig.java
+++ src/main/java/org/vasaka/springframework/batch/admin/config/WebConfig.java
@@ -1,8 +1,26 @@
 package org.vasaka.springframework.batch.admin.config;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.batch.admin.domain.support.ISO8601DateFormatWithMilliSeconds;
+import org.springframework.batch.admin.domain.support.VariableTypeJackson2ObjectMapperFactoryBean;
+import org.springframework.batch.admin.web.RestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.web.HateoasPageableHandlerMethodArgumentResolver;
+import org.springframework.data.web.HateoasSortHandlerMethodArgumentResolver;
+import org.springframework.data.web.PagedResourcesAssemblerArgumentResolver;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -10,23 +28,83 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
 @Configuration
 @EnableWebMvc
+@Import(RestConfiguration.class) // For loading spring batch admin's controller
 @ComponentScan(basePackages = "org.vasaka.springframework.batch.admin.controller")
+@EnableSpringDataWebSupport
 public class WebConfig extends WebMvcConfigurerAdapter {
-
 	@Bean
 	public ViewResolver viewResolver() {
 		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
 		viewResolver.setViewClass(JstlView.class);
 		viewResolver.setPrefix("/WEB-INF/views/");
 		viewResolver.setSuffix(".jsp");
-		return viewResolver;
 
+		return viewResolver;
 	}
-	
+
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+
+		argumentResolvers.add(sortResolver());
+		argumentResolvers.add(pageableResolver());
+		argumentResolvers.add(sortResolver());
+	}
+
+	@Bean
+	public HateoasSortHandlerMethodArgumentResolver sortResolver() {
+		return new HateoasSortHandlerMethodArgumentResolver();
+	}
+
+	@Bean
+	public PagedResourcesAssemblerArgumentResolver pagedResourcesAssemblerArgumentResolver() {
+		return new PagedResourcesAssemblerArgumentResolver(pageableResolver(), null);
+	}
+
+	@Bean
+	public HateoasPageableHandlerMethodArgumentResolver pageableResolver() {
+		return new HateoasPageableHandlerMethodArgumentResolver(sortResolver());
+	}
+
+	@Override
+	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+		converters.add(jsonConverter());
+	}
+
 	@Override
 	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
-		 configurer.enable();
+		configurer.enable();
 	}
+
+	@Bean
+	public MappingJackson2HttpMessageConverter jsonConverter() {
+		Jackson2ObjectMapperBuilder builder = Jackson2ObjectMapperBuilder.xml();
+		builder.indentOutput(true);
+		MappingJackson2HttpMessageConverter jacksonConverter = new MappingJackson2HttpMessageConverter(builder.build());
+		jacksonConverter.setSupportedMediaTypes(Arrays.asList(MediaType.valueOf("application/json")));
+		jacksonConverter.setObjectMapper(jacksonObjectMapper());
+		return jacksonConverter;
+	}
+
+	@Bean
+	public ObjectMapper jacksonObjectMapper() {
+		VariableTypeJackson2ObjectMapperFactoryBean factoryBean = new VariableTypeJackson2ObjectMapperFactoryBean();
+		factoryBean.setFeaturesToDisable(SerializationFeature.WRITE_DATE_KEYS_AS_TIMESTAMPS);
+		factoryBean.setDateFormat(ISO8601DateFormatWithMilliSeconds.getDateInstance());
+		Map<Class<?>, Class<?>> mixIns = new HashMap<Class<?>, Class<?>>();
+		mixIns.put(org.springframework.batch.core.JobParameters.class, org.springframework.batch.admin.domain.support.JobParametersJacksonMixIn.class);
+		mixIns.put(org.springframework.batch.core.JobParameter.class, org.springframework.batch.admin.domain.support.JobParameterJacksonMixIn.class);
+		mixIns.put(org.springframework.batch.admin.domain.StepExecutionHistory.class,
+				org.springframework.batch.admin.domain.support.StepExecutionHistoryJacksonMixIn.class);
+		mixIns.put(org.springframework.batch.core.ExitStatus.class, org.springframework.batch.admin.domain.support.ExitStatusJacksonMixIn.class);
+		factoryBean.setMixIns(mixIns);
+		factoryBean.afterPropertiesSet();
+		ObjectMapper objectMapper = factoryBean.getObject();
+		return objectMapper;
+	}
+
 }
