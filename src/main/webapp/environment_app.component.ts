c7d676d9774af82b06be830a292a5203de19ce38
Kaushik Dhruv <kaushikd.java@gmail.com>
2017-03-14 21:59:47 -0400

    Angular 2 env. files

---
+++ src/main/webapp/environment_app.component.ts
@@ -0,0 +1,13 @@
+import {Component, View} from "angular2/core";
+
+@Component({
+   selector: 'my-app'
+})
+
+@View({
+  template: '<h2>Vasaka Spring Batch Admin</h2>'
+})
+
+export class AppComponent {
+
+}
